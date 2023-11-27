53e0be1d921f6c955de1b535794b645c4a19ba76
Shivam Kumar <life.shivam2394@gmail.com>
2023-11-28 01:04:05 +0530

    Added Responsiveness and Routing -> Final Commmit

--- src/App.js
+++ src/App.js
@@ -15,14 +15,15 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-    <div className="App w-screen h-[110vh] bg-richblack-900 flex flex-col">
+    <div className="App w-screen h-screen overflow-auto bg-richblack-900 flex flex-col">
       <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
 
       <Routes>
         <Route path='/' element={<Home isLoggedIn={isLoggedIn}/>}/>
-        {/* <Route path='/StudyNotion' element={<Home isLoggedIn={isLoggedIn}/>}/> */}
+        <Route path='/StudyNotion' element={<Home isLoggedIn={isLoggedIn}/>}/>
         <Route path='/login' element={<Login setIsLoggedIn={setIsLoggedIn}/>}/>
         <Route path='/signup' element={<Signup setIsLoggedIn={setIsLoggedIn}/>}/>
+        <Route path='/*' element={<Home isLoggedIn={isLoggedIn}/>}/>
         <Route path='/dashboard' element={  
           <PrivateRoute isLoggedIn={isLoggedIn}>
             <Dashboard/>
